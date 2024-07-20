4b6308b7276edc2562356f851edfb6b90829ade0
DT_Ganga <ganga.gollakaram@dispatchtrack.com>
2024-07-20 12:48:05 +0530

    updated with validations in course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ApplicationRecord
+    validates :name,presence: true,length: {minimum: 2,maximum: 50}
+    validates :description,presence: true,length: {minimum: 2,maximum: 100}
+    validates :price,presence: true,length: {minimum: 2,maximum: 100}
 end
