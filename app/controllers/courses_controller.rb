dd9e3834eaab43484131c7fd5bdcc46ed2df72fa
DT_Ganga <ganga.gollakaram@dispatchtrack.com>
2024-07-21 22:35:28 +0530

    added restrictions for user and articles view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -12,7 +12,7 @@ class CoursesController < ApplicationController
     end
     def create
         @course = Course.new(course_param)
-        @course.user = User.first
+        @course.user = current_user
         if @course.save
             flash[:notice] = "Course was created successfully."
             redirect_to course_path(@course)
--- app/controllers/mainpage_controller.rb
+++ app/controllers/mainpage_controller.rb
@@ -1,4 +1,5 @@
 class MainpageController < ApplicationController
     def home
+     redirect_to courses_path if logged_in?
     end
 end
