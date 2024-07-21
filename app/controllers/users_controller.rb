46311be489e4409e500653983e3084c0c0cc7c3c
DT_Ganga <ganga.gollakaram@dispatchtrack.com>
2024-07-21 21:56:13 +0530

    updated with login and logout helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
     def index
         @users = User.all
     end
@@ -6,13 +7,10 @@ class UsersController < ApplicationController
         @user = User.new
     end
     def show
-        @user = User.find(params[:id])
     end
     def edit
-        @user = User.find(params[:id])
     end
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "updated the #{@user.username}, successfully "
             redirect_to @user
@@ -23,6 +21,7 @@ class UsersController < ApplicationController
      def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to courses_path
     else
@@ -33,5 +32,8 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
